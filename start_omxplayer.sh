fcac28ec9feb0ad8f92399ececce90fe4f42cef7
francesconistri84 <francesconistri84@gmail.com>
2016-02-16 23:22:31 +0100

    change start_omxplayer command

--- start_omxplayer.sh
+++ start_omxplayer.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-start_omx=$1
-
-let i=1
-while [ 1 ]; do
-	if [[ ! -f ${DIR}/p2ptv-pi.pid ]]; then
-		break
-	elif [[ -z `ps -p $(cat ${DIR}/p2ptv-pi.pid) | tail -n +2` ]] ; then
-		stop_playing
-		break
-	else
-		${start_omx}
-	fi
-	let i++
-	sleep 1
-done
+omxplayer --audio_fifo 10 --video_fifo 1 --audio_queue 1 --video_queue 1  http://127.0.0.1:3333/tv.asf
